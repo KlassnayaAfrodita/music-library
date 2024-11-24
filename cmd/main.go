1c411aa92bfdeef546ad9ceda3c68c3431a45890
KlassnayaAfrodita <evgeny.kazanjan@yandex.ru>
2024-11-25 02:24:33 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -6,22 +6,21 @@ import (
 
 	"music-library/internal/api"
 	"music-library/internal/database"
+	"music-library/internal/logger"
+	"music-library/internal/middleware"
 )
 
-// @title Example API
-// @version 1.0
-// @description This is a sample server for Swagger.
-// @host localhost:8080
-// @BasePath /
 func main() {
+	// Инициализация логгера
+	logger.Init()
+
 	// Подключение базы данных
 	database.ConnectDB()
 	defer database.DB.Close()
 
-	// Настройка роутера
 	router := api.SetupRouter()
+	router.Use(middleware.RequestLogger())
 
-	// Запуск сервера
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
