73622527c8c808bfb86bc496b8934d3da7e1a452
KlassnayaAfrodita <evgeny.kazanjan@yandex.ru>
2024-11-25 05:38:40 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	_ "music-library/docs" // Подключаем автоматически сгенерированные Swagger-документы
 
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 	"music-library/internal/api"
-	"music-library/internal/database"
 	"music-library/internal/logger"
 	"music-library/internal/middleware"
 )
@@ -15,13 +17,14 @@ func main() {
 	logger.Init()
 
 	// Подключение базы данных
-	database.ConnectDB()
-	defer database.DB.Close()
+	// database.ConnectDB()
+	// defer database.DB.Close()
 
 	router := api.SetupRouter()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Use(middleware.RequestLogger())
 
 	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+		fmt.Errorf("start router error")
 	}
 }
